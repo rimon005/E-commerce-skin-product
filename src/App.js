c421ce333f44718c573cb8c2edc93d3520443748
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-12-17 15:09:18 +0600

    create a beautiful slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero/>
+      <Slider />
     </div>
   );
 }
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { Pagination, Navigation } from 'swiper';
+
+// Import swiper styles
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import './Slider.css'
+
+// sliderData
+
+import { SliderProducts } from '../../data/products'
+
+
+const Slider = () => {
+    return (
+        <div className='s-container'>
+            <Swiper
+                className='mySwiper'
+                modules={[Pagination, Navigation]}
+                slidesPerView={3}
+                spaceBetween={40}
+                slidesPerGroup={1}
+                navigation={true}
+                loop={true}
+                loopFillGroupWithBlank={true}
+            >
+                {
+                    SliderProducts.map((slide, i) => (
+                        <SwiperSlide key={i}>
+                            <div className='left-s'>
+                                <div className='name'>
+                                    <span>
+                                        {slide.name}
+                                    </span>
+                                    <span>{slide.detail}</span>
+                                </div>
+                                <span>${slide.price}</span>
+                                <div>
+                                    Shop Now
+                                </div>
+                            </div>
+                            <img src={slide.img} alt="product" className='img-p' />
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    );
+};
+
+export default Slider;
